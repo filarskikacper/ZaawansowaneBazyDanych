1ecd6833b85b61dd3c7e5c85745a1f0c7be72631
Kacper Filarski <kacperkof9@gmail.com>
2026-04-08 14:22:18 +0200

    Lab-5 complete

---
+++ ZaawansowaneBazyDanych/SalesLT/Tables/ProductAttribute.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [SalesLT].[ProductAttribute] (
+    [ProductAttributeID] INT                                          IDENTITY (1, 1) NOT NULL,
+    [ProductID]          INT                                          NOT NULL,
+    [Attributes]         XML(CONTENT [dbo].[ProductAttributesSchema]) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ProductAttributeID] ASC),
+    FOREIGN KEY ([ProductID]) REFERENCES [SalesLT].[Product] ([ProductID])
+);
+
---
+++ ZaawansowaneBazyDanych/dbo/XMLSchemaCollections/ProductAttributesSchema.sql
@@ -0,0 +1,19 @@
+﻿CREATE XML SCHEMA COLLECTION [dbo].[ProductAttributesSchema]
+    AS N'<xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <xsd:element name="Attributes">
+    <xsd:complexType>
+      <xsd:complexContent>
+        <xsd:restriction base="xsd:anyType">
+          <xsd:sequence>
+            <xsd:element name="Weight" type="xsd:decimal" minOccurs="0" />
+            <xsd:element name="Color" type="xsd:string" minOccurs="0" />
+            <xsd:element name="Material" type="xsd:string" minOccurs="0" />
+            <xsd:element name="Name" type="xsd:string" minOccurs="0" />
+            <xsd:element name="WarrantyMonths" type="xsd:integer" minOccurs="0" />
+          </xsd:sequence>
+        </xsd:restriction>
+      </xsd:complexContent>
+    </xsd:complexType>
+  </xsd:element>
+</xsd:schema>';
+
